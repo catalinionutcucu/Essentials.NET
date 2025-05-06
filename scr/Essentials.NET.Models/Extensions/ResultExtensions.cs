887ba8a64fc1964157ad9cbc1f0d2c2f2ddae0c8
catalin <catalinionut.cucu@gmail.com>
2025-05-06 12:45:15 +0300

    feat: created initial project and added result object

---
+++ scr/Essentials.NET.Models/Extensions/ResultExtensions.cs
@@ -0,0 +1,111 @@
+﻿namespace Essentials.NET.Models.Extensions;
+
+#region value result extensions
+
+public static partial class ResultExtensions
+{
+    /// <summary>
+    /// Returns a value of type TMap based on the result state
+    /// </summary>
+    /// <returns>A value of type TMap</returns>
+    public static TMap Map<TMap, TResult, TError>(this Result<TResult, TError> result, Func<TResult, TMap> success, Func<TError, TMap> failure)
+    {
+        return result.IsSuccess ? success(result.Value) : failure(result.Error);
+    }
+
+    /// <summary>
+    /// Returns a value of type TMap based on the result state
+    /// </summary>
+    /// <returns>A value of type TMap</returns>
+    public static async Task<TMap> MapAsync<TMap, TResult, TError>(this Result<TResult, TError> result, Func<TResult, Task<TMap>> success, Func<TError, Task<TMap>> failure)
+    {
+        return result.IsSuccess ? await success(result.Value) : await failure(result.Error);
+    }
+
+    /// <summary>
+    /// Executes an action based on the result state
+    /// </summary>
+    public static void Execute<TResult, TError>(this Result<TResult, TError> result, Action<TResult> success, Action<TError> failure)
+    {
+        if (result.IsSuccess)
+        {
+            success(result.Value);
+        }
+        else
+        {
+            failure(result.Error);
+        }
+    }
+
+    /// <summary>
+    /// Executes an asynchronous action based on the result state
+    /// </summary>
+    public static async Task ExecuteAsync<TResult, TError>(this Result<TResult, TError> result, Func<TResult, Task> success, Func<TError, Task> failure)
+    {
+        if (result.IsSuccess)
+        {
+            await success(result.Value);
+        }
+        else
+        {
+            await failure(result.Error);
+        }
+    }
+}
+
+#endregion
+
+#region empty result extensions
+
+public static partial class ResultExtensions
+{
+    /// <summary>
+    /// Returns a value of type TMap based on the result state
+    /// </summary>
+    /// <returns>A value of type TMap</returns>
+    public static TMap Map<TMap, TError>(this Result<TError> result, Func<TMap> success, Func<TError, TMap> failure)
+    {
+        return result.IsSuccess ? success() : failure(result.Error);
+    }
+
+    /// <summary>
+    /// Returns a value of type TMap based on the result state
+    /// </summary>
+    /// <returns>A value of type TMap</returns>
+    public static async Task<TMap> MapAsync<TMap, TError>(this Result<TError> result, Func<Task<TMap>> success, Func<TError, Task<TMap>> failure)
+    {
+        return result.IsSuccess ? await success() : await failure(result.Error);
+    }
+
+    /// <summary>
+    /// Executes an action based on the result state
+    /// </summary>
+    public static void Execute<TError>(this Result<TError> result, Action success, Action<TError> failure)
+    {
+        if (result.IsSuccess)
+        {
+            success();
+        }
+        else
+        {
+            failure(result.Error);
+        }
+    }
+
+    /// <summary>
+    /// Executes an asynchronous action based on the result state
+    /// </summary>
+    public static async Task ExecuteAsync<TError>(this Result<TError> result, Func<Task> success, Func<TError, Task> failure)
+    {
+        if (result.IsSuccess)
+        {
+            await success();
+        }
+        else
+        {
+            await failure(result.Error);
+        }
+    }
+}
+
+#endregion
---
+++ scr/Essentials.NET.Models/Result.cs
@@ -0,0 +1,113 @@
+﻿namespace Essentials.NET.Models;
+
+#region value result
+
+public class Result<TResult, TError>
+{
+    public ResultState State { get; }
+
+    public TResult? Value { get; }
+
+    public TError? Error { get; }
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    protected Result(TResult value)
+    {
+        State = ResultState.Success;
+        Value = value;
+    }
+
+    protected Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref = "Result{TResult,TError}" /> representing a success result
+    /// </summary>
+    /// <returns>An instance of <see cref = "Result{TResult,TError}" /> representing a success result</returns>
+    public static Result<TResult, TError> Success(TResult value)
+    {
+        return new(value);
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref = "Result{TResult,TError}" /> representing a failure result
+    /// </summary>
+    /// <returns>An instance of <see cref = "Result{TResult,TError}" /> representing a failure result</returns>
+    public static Result<TResult, TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TResult, TError>(TResult value)
+    {
+        return Success(value);
+    }
+
+    public static implicit operator Result<TResult, TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+#endregion
+
+#region empty result
+
+public class Result<TError>
+{
+    public ResultState State { get; }
+
+    public TError? Error { get; }
+
+    public bool IsSuccess => State is ResultState.Success;
+
+    public bool IsFailure => State is ResultState.Failure;
+
+    protected Result()
+    {
+        State = ResultState.Success;
+    }
+
+    protected Result(TError error)
+    {
+        State = ResultState.Failure;
+        Error = error;
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref = "Result{TError}" /> representing a success result
+    /// </summary>
+    /// <returns>An instance of <see cref = "Result{TError}" /> representing a success result</returns>
+    public static Result<TError> Success()
+    {
+        return new();
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref = "Result{TError}" /> representing a failure result
+    /// </summary>
+    /// <returns>An instance of <see cref = "Result{TError}" /> representing a failure result</returns>
+    public static Result<TError> Failure(TError error)
+    {
+        return new(error);
+    }
+
+    public static implicit operator Result<TError>(TError error)
+    {
+        return Failure(error);
+    }
+}
+
+#endregion
+
+public enum ResultState
+{
+    Success,
+    Failure
+}
