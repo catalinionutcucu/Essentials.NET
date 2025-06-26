f1988d8fc1ad3f955e283c31d18a2da5ceb310a7
catalin <catalinionut.cucu@gmail.com>
2025-06-26 10:09:41 +0300

    feat: added extension methods for IEnumerable

--- src/Essentials.NET/Extensions/EnumerableExtensions.cs
+++ src/Essentials.NET/Extensions/EnumerableExtensions.cs
@@ -39,6 +39,31 @@ public static class EnumerableExtensions
     }
 
     /// <summary>
+    /// Determines whether the enumerable contains some elements that match the specified predicate.
+    /// </summary>
+    /// <returns><c>true</c> if the enumerable contains some elements that match the specified predicate otherwise <c>false</c>.</returns>
+    public static bool HasSome<TItem>(this IEnumerable<TItem> enumerable, Func<TItem, bool> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+
+        if (enumerable is null)
+        {
+            return false;
+        }
+
+        return enumerable.Any(predicate);
+    }
+
+    /// <summary>
+    /// Determines whether the enumerable contains some non-null elements.
+    /// </summary>
+    /// <returns><c>true</c> if the enumerable contains some non-null elements otherwise <c>false</c>.</returns>
+    public static bool HasSomeNotNull<TItem>(this IEnumerable<TItem> enumerable)
+    {
+        return enumerable.HasSome(item => item is not null);
+    }
+
+    /// <summary>
     /// Determines whether the enumerable is empty.
     /// </summary>
     /// <returns><c>true</c> if the enumerable is empty otherwise <c>false</c>.</returns>
@@ -55,5 +80,30 @@ public static class EnumerableExtensions
         }
 
         return !enumerable.Any();
+    }
+
+    /// <summary>
+    /// Determines whether the enumerable contains no elements that match the specified predicate.
+    /// </summary>
+    /// <returns><c>true</c> if the enumerable contains no elements that match the specified predicate otherwise <c>false</c>.</returns>
+    public static bool HasNone<TItem>(this IEnumerable<TItem> enumerable, Func<TItem, bool> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+
+        if (enumerable is null)
+        {
+            return true;
+        }
+
+        return !enumerable.Any(predicate);
+    }
+
+    /// <summary>
+    /// Determines whether the enumerable contains no non-null elements.
+    /// </summary>
+    /// <returns><c>true</c> if the enumerable contains no non-null elements otherwise <c>false</c>.</returns>
+    public static bool HasNoneNotNull<TItem>(this IEnumerable<TItem> enumerable)
+    {
+        return enumerable.HasNone(item => item is not null);
     }
 }
